8d8a521eacd4f4176aa5b5736f8fec0fa1266b9e
Hossam Magdy <hossam6236@gmail.com>
2016-02-08 21:03:21 +0200

    Update TorrentZ [load IMDB info].user.js

--- TorrentZ [load IMDB info].user.js
+++ TorrentZ [load IMDB info].user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name          TorrentZ [load IMDB info] - by hossam6236
+// @name          TorrentZ [load IMDB info]
 // @namespace     
 // @version       1.00
-// @description   Userscript (for Tampermonkey or Greasemonkey) for that show ALL movie/series info from IMDB on TorrentZ domains (title, rating, poster, ...)
+// @description   Show info of movie/series (rating, poster, actors, ...) from IMDB on TorrentZ domains (torrentz.eu, torrentz.com, ...)
 // @author        hossam6236
 // @license       GNU General Public License v3.0
 // @include       https://torrentz.eu/*
@@ -31,8 +31,8 @@
 // @include       https://www.bestdownload.eu/*
 // @include       https://torrentsmirror.com/*
 // @include       https://www.torrentsmirror.com/*
-// @require        https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
-// @grant          GM_xmlhttpRequest
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @grant         GM_xmlhttpRequest
 // ==/UserScript==
 
 // regext for cleaning the titles
@@ -50,47 +50,47 @@ try {
     var start_button = $('<button style="position: absolute; left: 500px; padding:10px;">  load info from IMDB  </button>'); //Equivalent: $(document.createElement('img'))\
     start_button.prependTo('body');
     var count = 0;
-    var scroll = 0
+    scroll_pos = 0;
     $(window).scroll(function (event) {
-        scroll = $(window).scrollTop();
+        scroll_pos = $(window).scrollTop();
     });
 
     var updateElement_dl = function(title, dl_element){
-        var el_title = movie_db[title].imdbVotes + ' votes - ' +  movie_db[title].Runtime + ' - Rated ' + movie_db[title].Rated + ' - Awards: ' +  movie_db[title].Awards
+        var el_title = movie_db[title].imdbVotes + ' votes - ' +  movie_db[title].Runtime + ' - Rated ' + movie_db[title].Rated + ' - Awards: ' +  movie_db[title].Awards;
         var star = '';
         if(movie_db[title].Awards.toLowerCase().indexOf('won') > -1 || movie_db[title].Awards.toLowerCase().indexOf('win') > -1){
-            var star = '&#9733;';
+            star = '&#9733;';
         }else if(movie_db[title].Awards.toLowerCase().indexOf('nominated') > -1 || movie_db[title].Awards.toLowerCase().indexOf('nomination') > -1){
-            var star = '&#9734;';
+            star = '&#9734;';
         }
         var el = '<a href="http://www.imdb.com/title/' + movie_db[title].imdbID + '" target="_blank" title="' + el_title + '">' + movie_db[title].imdbRating + star + '</a>';
-        $('a', dl_element).attr('title', movie_db[title].Title + ' (' +  movie_db[title].Year + ') - ' +  movie_db[title].Plot);
-        $('a', dl_element).after(' <b style="font-size:80%;">('+movie_db[title].Genre + ')</b>');
-        $('a', dl_element).on('mouseover', function(e){
+        $('a', dl_element).first().attr('title', movie_db[title].Title + ' (' +  movie_db[title].Year + ') - ' +  movie_db[title].Plot);
+        $('a', dl_element).first().after(' <b style="font-size:80%;">('+movie_db[title].Genre + ')</b>');
+        $('a', dl_element).first().on('mouseover', function(e){
             //poster_img.find('img').attr('src', "");
             poster_img.find('img').attr('src', movie_db[title].Poster);
             poster_img.find('#imdb_a').attr('href', 'http://www.imdb.com/title/' + movie_db[title].imdbID + '');
             poster_img.find('#imdb_a').attr('title', movie_db[title].Title + ' (' + movie_db[title].Year + ')');
-            poster_img.find('#imdb_title').text(movie_db[title].Title)
-            poster_img.find('#imdb_year').text(movie_db[title].Year)
-            poster_img.find('#imdb_rating').text(movie_db[title].imdbRating)
-            poster_img.find('#imdb_votes').text(movie_db[title].imdbVotes)
-            poster_img.find('#imdb_rated').text(movie_db[title].Rated)
-            poster_img.find('#imdb_runtime').text(movie_db[title].Runtime)
-            poster_img.find('#imdb_awards').text(movie_db[title].Awards)
-            poster_img.find('#imdb_genre').text(movie_db[title].Genre)
-            poster_img.find('#imdb_actors').text(movie_db[title].Actors)
-            poster_img.find('#imdb_director').text(movie_db[title].Director)
-            poster_img.find('#imdb_writer').text(movie_db[title].Writer)
-            poster_img.find('#imdb_plot').text(movie_db[title].Plot)
+            poster_img.find('#imdb_title').text(movie_db[title].Title);
+            poster_img.find('#imdb_year').text(movie_db[title].Year);
+            poster_img.find('#imdb_rating').text(movie_db[title].imdbRating);
+            poster_img.find('#imdb_votes').text(movie_db[title].imdbVotes);
+            poster_img.find('#imdb_rated').text(movie_db[title].Rated);
+            poster_img.find('#imdb_runtime').text(movie_db[title].Runtime);
+            poster_img.find('#imdb_awards').text(movie_db[title].Awards);
+            poster_img.find('#imdb_genre').text(movie_db[title].Genre);
+            poster_img.find('#imdb_actors').text(movie_db[title].Actors);
+            poster_img.find('#imdb_director').text(movie_db[title].Director);
+            poster_img.find('#imdb_writer').text(movie_db[title].Writer);
+            poster_img.find('#imdb_plot').text(movie_db[title].Plot);
             poster_img.css({
                 left:  $(window).width()*2/3 - poster_img.width()/2, //e.pageX + 100,
-                top:   scroll + $(window).height()/2 - poster_img.height()/2
+                top:   scroll_pos + $(window).height()/2 - poster_img.height()/2
             });
             //poster_img.attr('title','Actors: ' + movie_db[title].Actors + ' - Director: ' + movie_db[title].Director) 
             poster_img.show();
         });
-        $('a', dl_element).on('mouseout', function(e){
+        $('a', dl_element).first().on('mouseout', function(e){
             poster_img.hide();
         });
         //$('b', this).attr('style','font-size:65%;')
@@ -141,6 +141,7 @@ try {
     
     $(document).ready(function() {});
     start_button.click(function() {
+        start_button.hide();
         poster_img = $('<table style="position: absolute; width:450px; height:283px; background-color:white; border:2px solid black; border-collapse: collapse; border-spacing:0px; cell-spacing:0px;"><tr>\
 <td><a href="" target="_blank" title="" id="imdb_a" style="display:flex;"><img width="200" height="283" border="0"></a></td>\
 <td style="border:1px solid black;"><div style="text-align:left; padding:3px; font-size:10pt; font-family:Tahoma; height:100%; overflow:auto;">\
