8ab9b7a1ab2fe2cbfb31c46801eb17c76ff1f5e8
Hossam Magdy <hossam6236@gmail.com>
2017-09-18 21:59:47 +0200

    Add subscene

--- IMDB info + .torrent from magnet.user.js
+++ IMDB info + .torrent from magnet.user.js
@@ -22,6 +22,7 @@
 // @include       http*://*kickass*.*/*
 // @include       http*://*kat*.*/*
 // @include       http*://*boxofficemojo*.*/*
+// @include       http*://*subscene*.*/*
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @grant         GM_xmlhttpRequest
 // @connect       imdb.com
